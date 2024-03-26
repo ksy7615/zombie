341627995f2e8c36d9d6a6bdaef2194070432d51
ksy7615 <sykim7615@daum.net>
2024-03-26 17:36:40 +0900

    Add movePlayer Method Of Class Game

--- zombie/src/zombie/Game.java
+++ zombie/src/zombie/Game.java
@@ -6,13 +6,13 @@ public class Game {
 	private Player player = new Player(1, 200, 30, 2);
 	private Zombie zombie = new Zombie(4, 100, 10);
 	private Boss boss = new Boss(9, 300, 20, 100);
-	
+
 	private Scanner scanner = new Scanner(System.in);
 	private final int DESTINATION = 10;
 	private boolean isExit;
 
 	private Game() {
-		
+
 	}
 
 	private static Game instance = new Game();
@@ -21,53 +21,58 @@ public class Game {
 		return instance;
 	}
 	// >> 여기까지 싱글톤 처리
-	
+
 	private boolean isRun() {
-		if(player.getHp() == 0) {
+		if (player.getHp() == 0) {
 			return false;
 		}
-		
-		if(player.getPosition() == DESTINATION) {
+
+		if (player.getPosition() == DESTINATION) {
 			return false;
 		}
-		
-		if(isExit) {
+
+		if (isExit) {
 			return false;
 		}
-		
+
 		return true;
 	}
-	
+
 	private void locatedPlayer() {
-		System.out.println("플레이어의 위치 : "+player.getPosition());
+		System.out.println("플레이어의 위치 : " + player.getPosition());
 	}
-	
+
 	private void printMenu() {
 		System.out.println("[1] 앞으로 이동");
 		System.out.println("[0] 종료");
 	}
-	
+
+	private void movePlayer() {
+		int position = player.getPosition() + 1;
+		player.setPosition(position);
+	}
+
 	private void runMenu(int select) {
-		if(select == 1) {
-//			movePlayer();
+		if (select == 1) {
+			movePlayer();
 		} else if (select == 0) {
 			isExit = true;
 		}
 	}
 
 	public void run() {
-		while(isRun()) {
+		while (isRun()) {
 			locatedPlayer();
 			printMenu();
 			int select = inputNumber("입력");
 			runMenu(select);
 		}
 	}
-	
+
 	private int inputNumber(String message) {
 		int number = -1;
 		try {
-			System.out.println(message + " : ");
+			System.out.print(message + " : ");
 			String input = scanner.next();
 			number = Integer.parseInt(input);
 		} catch (Exception e) {
