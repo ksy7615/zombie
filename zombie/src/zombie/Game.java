b9c17342098a516e992635ad20ad506f55202298
ksy7615 <sykim7615@daum.net>
2024-03-26 17:21:56 +0900

    Add isRun Method Of Class Game

--- zombie/src/zombie/Game.java
+++ zombie/src/zombie/Game.java
@@ -4,6 +4,8 @@ public class Game {
 	private Player player = new Player(1, 200, 30, 2);
 	private Zombie zombie = new Zombie(4, 100, 10);
 	private Boss boss = new Boss(9, 300, 20, 100);
+	
+	private final int DESTINATION = 10;
 
 	private Game() {
 
@@ -13,6 +15,18 @@ public class Game {
 
 	public static Game getInstance() {
 		return instance;
+	}
+	
+	private boolean isRun() {
+		if(player.getHp() == 0) {
+			return false;
+		}
+		
+		if(player.getPosition() == DESTINATION) {
+			return false;
+		}
+		
+		return true;
 	}
 
 	public void run() {
