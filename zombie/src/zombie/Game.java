524d87caff3f7ca940dd3d3d3ca94252c1fcbbb1
ksy7615 <sykim7615@daum.net>
2024-03-26 17:09:54 +0900

    Add Setting Player & Mob Of Class Game

--- zombie/src/zombie/Game.java
+++ zombie/src/zombie/Game.java
@@ -7,11 +7,14 @@ public class Game {
 	}
 	
 	private static Game instance = new Game();
+	
 	public static Game getInstance() {
 		return instance;
 	}
 	
 	public void run() {
-		
+		Player player = new Player(1, 200, 30, 2);
+		Zombie zombie = new Zombie(4, 100, 10);
+		Boss boss = new Boss(9, 300, 20, 100);
 	}
 }
