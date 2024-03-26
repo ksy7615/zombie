ae80d5383211075a4e886aa7e9b1dee4a53b0910
ksy7615 <sykim7615@daum.net>
2024-03-26 16:24:52 +0900

    Modify sysout print Of Class Zombie

--- zombie/src/zombie/Zombie.java
+++ zombie/src/zombie/Zombie.java
@@ -19,7 +19,7 @@ public class Zombie extends Unit {
 		}
 		System.out.println("좀비가 공격했다!");
 		System.out.printf("[%d]데미지의 깨물기 공격\n", this.bite);
-		System.out.println(player);
+		System.out.println("player" + player);
 	}
-
+	
 }
