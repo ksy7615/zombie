ab27d4a8eafa545d9c262e6cc9be8acdd8233361
ksy7615 <sykim7615@daum.net>
2024-03-26 17:19:13 +0900

    Modify Setting location Of Class Game

--- zombie/src/zombie/Game.java
+++ zombie/src/zombie/Game.java
@@ -1,25 +1,21 @@
 package zombie;
 
 public class Game {
-	
+	private Player player = new Player(1, 200, 30, 2);
+	private Zombie zombie = new Zombie(4, 100, 10);
+	private Boss boss = new Boss(9, 300, 20, 100);
+
 	private Game() {
-		
+
 	}
-	
+
 	private static Game instance = new Game();
-	
+
 	public static Game getInstance() {
 		return instance;
 	}
-	
-	private void setGame() {
-		Player player = new Player(1, 200, 30, 2);
-		Zombie zombie = new Zombie(4, 100, 10);
-		Boss boss = new Boss(9, 300, 20, 100);
-	}
-	
+
 	public void run() {
-		setGame();
 		
 	}
 }
