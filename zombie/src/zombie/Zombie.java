e681cb359959f1f0ab8d11e782b10d631903d7c0
ksy7615 <sykim7615@daum.net>
2024-03-26 16:19:44 +0900

    Add Attack Method Of Class Zombie

--- zombie/src/zombie/Zombie.java
+++ zombie/src/zombie/Zombie.java
@@ -12,6 +12,14 @@ public class Zombie extends Unit {
 	public void attack(Unit player) {
 		bite = random.nextInt(attackMax)+1;
 		
+		player.setHp(player.getHp()-bite);
+		
+		if(player.getHp() <= 0) {
+			player.setHp(0);
+		}
+		System.out.println("좀비가 공격했다!");
+		System.out.printf("[%d]데미지의 깨물기 공격\n", this.bite);
+		System.out.println(player);
 	}
 
 }
