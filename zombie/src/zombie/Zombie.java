4405326fff3b9bd17e927dee95e5966c150a0cd6
ksy7615 <sykim7615@daum.net>
2024-03-26 14:10:23 +0900

    Add Constructor & override attack method Of class Zombie

---
+++ zombie/src/zombie/Zombie.java
@@ -0,0 +1,14 @@
+package zombie;
+
+public class Zombie extends Unit {
+
+	public Zombie(int position, int hp, int attack) {
+		super(position, hp, attack);
+	}
+
+	@Override
+	public void attack(Unit player) {
+		
+	}
+
+}
