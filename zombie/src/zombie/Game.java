1c8e368e1714dcef7a1d4444f061c3f5017f5a4b
ksy7615 <sykim7615@daum.net>
2024-03-26 17:25:54 +0900

    Modify isRun Method & Add printMenu Method Of Class Game

--- zombie/src/zombie/Game.java
+++ zombie/src/zombie/Game.java
@@ -1,14 +1,18 @@
 package zombie;
 
+import java.util.Scanner;
+
 public class Game {
 	private Player player = new Player(1, 200, 30, 2);
 	private Zombie zombie = new Zombie(4, 100, 10);
 	private Boss boss = new Boss(9, 300, 20, 100);
 	
+	private Scanner scanner = new Scanner(System.in);
 	private final int DESTINATION = 10;
+	private boolean isExit;
 
 	private Game() {
-
+		
 	}
 
 	private static Game instance = new Game();
@@ -16,6 +20,7 @@ public class Game {
 	public static Game getInstance() {
 		return instance;
 	}
+	// >> 여기까지 싱글톤 처리
 	
 	private boolean isRun() {
 		if(player.getHp() == 0) {
@@ -26,10 +31,21 @@ public class Game {
 			return false;
 		}
 		
+		if(isExit) {
+			return false;
+		}
+		
 		return true;
+	}
+	
+	private void printMenu() {
+		System.out.println("[1] 앞으로 이동");
+		System.out.println("[0] 종료");
 	}
 
 	public void run() {
-		
+		while(isRun()) {
+			
+		}
 	}
 }
