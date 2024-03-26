69375a0a4cb5456f212873ce50feca077ad4cfec
ksy7615 <sykim7615@daum.net>
2024-03-26 15:09:47 +0900

    Modify Constructor Of Class Boss

--- zombie/src/zombie/Boss.java
+++ zombie/src/zombie/Boss.java
@@ -3,8 +3,9 @@ package zombie;
 public class Boss extends Zombie {
 	private int shield;
 
-	public Boss(int position, int hp, int attackMax) {
+	public Boss(int position, int hp, int attackMax, int shield) {
 		super(position, hp, attackMax);
+		this.shield = shield;
 	}
 
 }
