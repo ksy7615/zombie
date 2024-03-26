a4411e47e6039c2b1d50bb1b0fd2ded26f3b1d7b
ksy7615 <sykim7615@daum.net>
2024-03-26 13:51:40 +0900

    Add run Method in main Method Of Class ZombieGame

--- zombie/src/zombie/ZombieGame.java
+++ zombie/src/zombie/ZombieGame.java
@@ -3,8 +3,11 @@ package zombie;
 public class ZombieGame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ZombieGame.run();
+	}
+	
+	public static void run() {
+		Game.getInstance().run();
 	}
 
 }
