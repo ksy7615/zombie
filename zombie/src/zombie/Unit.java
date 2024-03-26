70d6ddb726fd620ea8d19bcff85641add5f4bda4
ksy7615 <sykim7615@daum.net>
2024-03-26 14:01:35 +0900

    Add Getter&Setter Of Class Unit

--- zombie/src/zombie/Unit.java
+++ zombie/src/zombie/Unit.java
@@ -7,4 +7,31 @@ abstract public class Unit {
 	// 2. hp
 	// 3. 공격력
 	
+	public Unit(int position, int hp, int attack) {
+		this.position = position;
+		this.hp = hp;
+		this.attack = attack;
+	}
+
+	public int getPosition() {
+		return this.position;
+	}
+
+	public void setPosition(int position) {
+		this.position = position;
+	}
+
+	public int getHp() {
+		return this.hp;
+	}
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+	public int getAttack() {
+		return this.attack;
+	}
+	
+	
 }
