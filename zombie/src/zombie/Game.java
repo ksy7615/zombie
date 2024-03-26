c9f3c94c464e74a5de091e56f7b00b02be9010b3
ksy7615 <sykim7615@daum.net>
2024-03-26 13:48:22 +0900

    Create Singleton Pattern Of class Game

---
+++ zombie/src/zombie/Game.java
@@ -0,0 +1,17 @@
+package zombie;
+
+public class Game {
+	
+	private Game() {
+		
+	}
+	
+	private static Game instance = new Game();
+	public static Game getInstance() {
+		return instance;
+	}
+	
+	public void run() {
+		
+	}
+}
