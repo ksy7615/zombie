3f8c0e20894f48d8519c343ad2ff78af6784be27
ksy7615 <sykim7615@daum.net>
2024-03-26 14:29:24 +0900

    Add Attack Strength Setting Of Class Zombie

--- zombie/src/zombie/Zombie.java
+++ zombie/src/zombie/Zombie.java
@@ -4,11 +4,13 @@ public class Zombie extends Unit {
 	private int bite;
 
 	public Zombie() {
+		// 위치 / hp / 최대 공격력
 		super(5, 100, 10);
 	}
 
 	@Override
 	public void attack(Unit player) {
+		bite = random.nextInt(attackMax)+1;
 		
 	}
 
