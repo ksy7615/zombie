e1e01c901d902c612499d68f22e9c7dd1f313c17
ksy7615 <sykim7615@daum.net>
2024-03-26 15:37:01 +0900

    Add attack Method Of Class Player

--- zombie/src/zombie/Player.java
+++ zombie/src/zombie/Player.java
@@ -11,7 +11,33 @@ public class Player extends Unit {
 
 	@Override
 	public void attack(Unit monster) {
-		
+		if(monster instanceof Boss) {
+			Boss boss = (Boss) monster;
+			
+			sword = random.nextInt(attackMax) + 1;
+			// 보스는 쉴드가 있으므로 공격할 때 쉴드부터 깨기
+			if(boss.getShield() > 0) {
+				int brokenShield = boss.getShield() - sword;
+				
+				// 쉴드가 안깨진 상태
+				if(brokenShield >= 0) {
+					boss.setShield(brokenShield);
+					// 쉴드 깨지면서 유효 공격
+				} else {
+					boss.setShield(0);
+					boss.setHp(boss.getHp() - brokenShield);
+				}
+			} else {
+				boss.setHp(boss.getHp()-sword);
+			}
+			
+			if(boss.getHp() <= 0) {
+				boss.setHp(0);
+			}
+			System.out.printf("플레이어가 공격력 %d로 공격\n", sword);
+			System.out.printf("현재 보스의 Shield : %d\n", boss.getShield());
+			System.out.printf("현재 보스의 HP : %d\n", boss.getHp());
+		}
 	}
 
 }
