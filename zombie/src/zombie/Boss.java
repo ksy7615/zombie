9f8aa0a5d0116f445a7b942485391ed747c8446b
ksy7615 <sykim7615@daum.net>
2024-03-27 10:13:36 +0900

    Refactor // Separate Method Of Class Boss

--- zombie/src/zombie/Boss.java
+++ zombie/src/zombie/Boss.java
@@ -18,32 +18,40 @@ public class Boss extends Zombie implements BossMob {
 
 	@Override
 	public void attack(Unit player) {
-
-		// 궁극기 설정 (25%)
+		// 궁극기 설정 (확률 25%)
 		int ultimateChance = random.nextInt(4) + 1;
 		if (ultimateChance == 1) {
-			System.out.println("Boss의 궁극기 사용 *공격력 2배*");
-			int power = 2 * (random.nextInt(attackMax) + 1);
-
-			player.setHp(player.getHp() - power);
-
-			if (player.getHp() <= 0) {
-				player.setHp(0);
-			}
-			System.out.println("보스가 공격했다!");
-			System.out.printf("[%d]데미지의 공격\n", power);
-			System.out.println("치명상을 입었다!");
-			System.out.println("player" + player);
+			ultimate(player);
+		// 일반 공격	
 		} else {
-			int power = random.nextInt(attackMax) + 1;
-			player.setHp(player.getHp() - power);
-
-			if (player.getHp() <= 0) {
-				player.setHp(0);
-			}
-			System.out.println("보스가 공격했다!");
-			System.out.printf("[%d]데미지의 공격\n", power);
-			System.out.println("player" + player);
+			generalAttack(player);
 		}
+	}
+
+	private void ultimate(Unit player) {
+		System.out.println("Boss의 궁극기 사용 *공격력 2배*");
+		int power = 2 * (random.nextInt(attackMax) + 1);
+
+		player.setHp(player.getHp() - power);
+
+		if (player.getHp() <= 0) {
+			player.setHp(0);
+		}
+		System.out.println("보스가 공격했다!");
+		System.out.printf("[%d]데미지의 공격\n", power);
+		System.out.println("치명상을 입었다!");
+		System.out.println("player" + player);
+	}
+	
+	private void generalAttack(Unit player) {
+		int power = random.nextInt(attackMax) + 1;
+		player.setHp(player.getHp() - power);
+
+		if (player.getHp() <= 0) {
+			player.setHp(0);
+		}
+		System.out.println("보스가 공격했다!");
+		System.out.printf("[%d]데미지의 공격\n", power);
+		System.out.println("player" + player);
 	}
 }
